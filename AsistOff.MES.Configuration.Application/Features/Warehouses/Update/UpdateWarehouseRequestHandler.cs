649e8abd7790968b2800bc7056a1e768268bf0b4
oskar <oskar.mokrzycki@gmail.com>
2025-07-07 16:07:32 +0200

    auth

--- AsistOff.MES.Configuration.Application/Features/Warehouses/Update/UpdateWarehouseRequestHandler.cs
+++ AsistOff.MES.Configuration.Application/Features/Warehouses/Update/UpdateWarehouseRequestHandler.cs
@@ -1,13 +1,27 @@
-﻿using ErrorOr;
+﻿using AsistOff.MES.Configuration.Domain.Errors;
+using AsistOff.MES.Configuration.Domain.Repositories;
+using ErrorOr;
 using MediatR;
 
 namespace AsistOff.MES.Configuration.Application.Features.Warehouses.Update;
 
-internal sealed class UpdateWarehouseRequestHandler
+internal sealed class UpdateWarehouseRequestHandler(
+    IWarehousesRepository warehousesRepository)
     : IRequestHandler<UpdateWarehouseRequest, ErrorOr<Unit>>
 {
-    public Task<ErrorOr<Unit>> Handle(UpdateWarehouseRequest request, CancellationToken cancellationToken)
+    public async Task<ErrorOr<Unit>> Handle(UpdateWarehouseRequest request, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var warehouse = await warehousesRepository.GetByIdAsync(request.Id, cancellationToken);
+
+        if (warehouse is null)
+        {
+            return Errors.Warehouses.NotFoundError;
+        }
+        
+        warehouse.Name = request.Name;
+        
+        await warehousesRepository.UpdateAsync(warehouse, cancellationToken);
+
+        return Unit.Value;
     }
 }
